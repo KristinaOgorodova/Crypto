013ff10e0512c402c7fc478bcc01de78ec9c7ce7
KristinaOgorodova <Skrok@inbox.ru>
2022-07-21 14:51:53 +0300

    добавления2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
-const headerNavBtn= document.querySelector('.header__nav-btn');
-const headerNav=document.querySelector('.header__navigation');
+import './menu.js'
+import './modal.js'
 
-headerNavBtn.addEventListener('click',()   =>{
-    headerNav.classList.toggle('header__navigation_open')
-});
+
+
